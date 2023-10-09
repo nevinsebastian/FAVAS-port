71531eef64baa26469ccd067e89df30eb2d4d711
nevin <nevin@pop-os.localdomain>
2023-10-09 15:27:07 +0530

    basic

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Banner from './components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner />
+      <Skills/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
